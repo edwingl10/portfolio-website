81d36323db6cac3beb0e383ef5c06bacf52d540d
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2022-09-05 11:39:30 -0700

    underline for current tab

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { AppBar, Tabs, Tab, Toolbar } from '@mui/material';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const links = {
   About: '/about',
@@ -7,13 +8,30 @@ const links = {
 };
 
 export default function Navbar() {
+  const router = useRouter();
+  let currentTab = false;
+
+  Object.entries(links).forEach(([title, link]) => {
+    if (router.pathname === link) {
+      currentTab = title;
+    }
+  });
+
   return (
-    <AppBar>
+    <AppBar color="inherit">
       <Toolbar>
-        <Tabs>
+        <Tabs aria-label="navbar tabs" value={currentTab}>
           {Object.entries(links).map(([title, link]) => (
-            <Link href={link} passHref key={title}>
-              <Tab label={title} component="a" />
+            <Link href={link} passHref key={title} value={title}>
+              <Tab
+                label={title}
+                component="a"
+                sx={{
+                  textTransform: 'none',
+                  minWidth: { xs: 64, md: 72 },
+                  p: 0,
+                }}
+              />
             </Link>
           ))}
         </Tabs>
