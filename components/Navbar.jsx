7c1ddb64353d6290616d3e46ec5d5902b18feadc
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2022-09-05 16:20:56 -0700

    logo added and right aligned links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
-import { AppBar, Tabs, Tab, Toolbar } from '@mui/material';
+import { AppBar, Tabs, Tab, Toolbar, SvgIcon } from '@mui/material';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import Icon from './Icon';
 
 const links = {
   About: '/about',
@@ -20,7 +21,13 @@ export default function Navbar() {
   return (
     <AppBar color="inherit">
       <Toolbar>
-        <Tabs aria-label="navbar tabs" value={currentTab}>
+        <Link href="/">
+          <SvgIcon fontSize="large" color="primary" sx={{ cursor: 'pointer' }}>
+            <Icon name="logo" height="100%" width="100%" />
+          </SvgIcon>
+        </Link>
+
+        <Tabs aria-label="navbar tabs" value={currentTab} sx={{ ml: 'auto' }}>
           {Object.entries(links).map(([title, link]) => (
             <Link href={link} passHref key={title} value={title}>
               <Tab
