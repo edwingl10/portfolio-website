061db304ed4a3935488a7d843a5f3e8502544b27
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2022-10-31 16:37:57 -0700

    favicon added

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,7 +11,29 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href="/favicon.ico" />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3d5a80" />
+          <meta name="msapplication-TileColor" content="#3d5a80" />
+          <meta name="theme-color" content="#ffffff" />
+
           <link
             href="https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;700&display=swap"
             rel="stylesheet"
