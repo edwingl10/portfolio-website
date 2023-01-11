164165c0baf1ea828be6e99f115fed124065900c
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2023-01-11 10:14:38 -0800

    projects page head translation

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -9,13 +9,11 @@ import ProjectSection from '../../components/ProjectSection';
 import SEOHead from '../../components/SEOHead';
 
 export default function ListProjects({ placeholders, bannerBlur }) {
-  const title = 'Edwin Lopez | Projects';
-  const description =
-    "A showcase of Edwin's various web / mobile projects and computer science programs.";
-  const keywords =
-    'projects, mobile development, web development, frameworks, software engineer, front end, back end';
-
   const { t } = useTranslation('projects');
+
+  const title = t('head.title');
+  const description = t('head.description');
+  const keywords = t('head.keywords');
 
   return (
     <>
