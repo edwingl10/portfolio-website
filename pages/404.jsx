b3eba2a76c9de7d4167efe9bc268db02ad2ee531
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2023-01-18 20:34:56 -0800

    Image and text added

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,7 +1,30 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Image from 'next/image';
+import { Button, Container, Typography, Box } from '@mui/material';
 
 export default function NotFound() {
-  return <h1>Page cannot be found...</h1>;
+  return (
+    <Box textAlign="center">
+      <Container sx={{ my: 5, py: 2 }}>
+        <Image
+          priority
+          src="/images/404.png"
+          alt="A laptop showing a 404 message"
+          width={700}
+          height={480}
+        />
+        <Typography variant="h3" paragraph>
+          Page Not Found
+        </Typography>
+        <Typography variant="h5" paragraph>
+          The page you are looking for is not available.
+        </Typography>
+        <Button variant="contained" color="secondary">
+          go home
+        </Button>
+      </Container>
+    </Box>
+  );
 }
 
 export async function getStaticProps({ locale }) {
